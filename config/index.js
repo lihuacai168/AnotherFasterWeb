5e0e1ae589388b8d21f99c36917db33b574f8ed2
Rikasai_wbiao <lihuacai@wbiao.cn>
2019-04-03 12:01:40 +0800

    访问地址改成0.0.0.0

--- config/index.js
+++ config/index.js
@@ -21,7 +21,7 @@ module.exports = {
         },
 
         // Various Dev Server settings
-        host: 'localhost', // can be overwritten by process.variables.HOST
+        host: '0.0.0.0', // can be overwritten by process.variables.HOST
         port: 8080, // can be overwritten by process.variables.PORT, if port is in use, a free one will be determined
         autoOpenBrowser: false,
         errorOverlay: true,
