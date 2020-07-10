0d3ea532783006651e85fe38b2255156e4a0b728
lihuacai <lihuacai168@gmail.com>
2020-07-10 13:29:24 +0800

    1.修改API状态的名称: 手动成功 -> 调试成功

--- src/pages/fastrunner/api/components/ApiList.vue
+++ src/pages/fastrunner/api/components/ApiList.vue
@@ -30,7 +30,7 @@
                             <i class="el-icon-arrow-down el-icon--right"></i>
                         </el-button>
                         <el-dropdown-menu slot="dropdown">
-                            <el-dropdown-item command="1">手动成功</el-dropdown-item>
+                            <el-dropdown-item command="1">调试成功</el-dropdown-item>
                             <el-dropdown-item command="0">还未调试</el-dropdown-item>
                             <el-dropdown-item command="2">调试失败</el-dropdown-item>
                             <el-dropdown-item command="3">自动成功</el-dropdown-item>
