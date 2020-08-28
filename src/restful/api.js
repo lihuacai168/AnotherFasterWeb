852795c6a7ead2f8334e31ae5ed549c41e4b997f
huangqiaoping <huangqiaoping@baibu.la>
2020-08-28 18:00:40 +0800

    feat: 新增ResContent组件.

--- src/restful/api.js
+++ src/restful/api.js
@@ -5,7 +5,9 @@ import {Message} from 'element-ui';
 
 
 
-export let baseUrl = "http://localhost:8000";
+// export let baseUrl = "http://localhost:8000";
+export let baseUrl = "http://192.168.17.107:8000";
+
 if( process.env.NODE_ENV === "production" ){
     baseUrl = "http://192.168.17.107:8000";
 }
