9f928f43a6024659d0e44d066edec7ab63327ce2
lihuacai <lihuacai168@gmail.com>
2021-08-09 16:37:29 +0800

    refactor: 重构Vue的日期和时间转换filter

--- src/main.js
+++ src/main.js
@@ -15,7 +15,7 @@ import 'styles/home.css'
 import 'styles/reports.css'
 import * as api from './restful/api'
 import store from './store'
-
+import {datetimeObj2str, timestamp2time} from './util/format.js'
 Vue.config.productionTip = false
 Vue.use(ElementUI)
 Vue.prototype.$api = api
@@ -23,53 +23,9 @@ Vue.prototype.$echarts = echarts
 Vue.use(VJsoneditor)
 Vue.use(VueClipboard)
 
-Vue.filter('datetimeFormat', function (time, format = 'YY-MM-DD hh:mm:ss') {
-    let date = new Date(time);
-    let year = date.getFullYear(),
-        month = date.getMonth() + 1,
-        day = date.getDate(),
-        hour = date.getHours(),
-        min = date.getMinutes(),
-        sec = date.getSeconds();
-    let preArr = Array.apply(null, Array(10)).map(function (elem, index) {
-        return '0' + index;
-    });
+Vue.filter('datetimeFormat', datetimeObj2str);
+Vue.filter("timestampToTime", timestamp2time);
 
-    let newTime = format.replace(/YY/g, year)
-        .replace(/MM/g, preArr[month] || month)
-        .replace(/DD/g, preArr[day] || day)
-        .replace(/hh/g, preArr[hour] || hour)
-        .replace(/mm/g, preArr[min] || min)
-        .replace(/ss/g, preArr[sec] || sec);
-
-    return newTime;
-});
-
-Vue.filter("timestampToTime", function (timestamp) {
-    if (!timestamp) {
-        return ''
-    }
-    let date = new Date(timestamp * 1000);
-    const Y = date.getFullYear() + '-';
-
-    // js的月份从0开始
-    const month = date.getMonth() + 1;
-    const M = (month < 10 ? '0' + month : month) + '-';
-
-    const days = date.getDate();
-    const D = (days + 1 < 10 ? '0' + days : days) + ' ';
-
-    const hours = date.getHours();
-    const h = (hours + 1 < 10 ? '0' + hours : hours) + ':';
-
-    const minutes = date.getMinutes();
-    const m = (minutes + 1 < 10 ? '0' + minutes : minutes) + ':';
-
-    const seconds = date.getSeconds();
-    const s = seconds + 1 < 10 ? '0' + seconds : seconds;
-
-    return Y + M + D + h + m + s;
-});
 Vue.prototype.setLocalValue = function (name, value) {
     if (window.localStorage) {
         localStorage.setItem(name, value);
